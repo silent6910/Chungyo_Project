89729b4ae83328982bea281cd919565ae6ea8fc6
silent <mailforjob6910@gmail.com>
2016-07-29 09:34:03 +0000

    修正改成PDO之後，造成無論如何都能註冊成功的錯誤

--- controllers/registerController.php
+++ controllers/registerController.php
@@ -11,14 +11,14 @@
         {
             if(!isset($_POST['Account'])||!isset($_POST['Password']))
                 die("請輸入正確");
-            else if(!preg_match("/^([0-9A-Za-z]+)$/",$_POST['Account'])) //如果帳號不為英文或數字則告知client錯誤
+            else if(!preg_match("/^([0-9A-Za-z]+)$/",$_POST['Account'])) 
+            //如果帳號不為英文或數字則告知client錯誤
             {
                 echo json_encode("no");
                 die();
             }
             $register=$this->model("register");
-            $row=$register->register_verify()->fetch();
-            if($row['Account']!='') 
+            if($register->register_verify()->rowCount()>0) 
                 echo json_encode(false);
             else{
                 $register->register();
