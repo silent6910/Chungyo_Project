f57a904fcd1bc9d56d16b6e331c47e0d666e88d6
silent <mailforjob6910@gmail.com>
2016-07-29 10:21:50 +0000

    新增此core的註解

--- core/App.php
+++ core/App.php
@@ -6,14 +6,14 @@ class App {
         $url = $this->parseUrl();
         
         $controllerName = "{$url[0]}Controller";
-        if (!file_exists("controllers/$controllerName.php"))
+        if (!file_exists("controllers/$controllerName.php")) //若此Controller不存在，則導向本人自制的404網頁
         {
             require_once(dirname(__FILE__)."/error404.html");
             return;
         }
         require_once "controllers/$controllerName.php";
         $controller = new $controllerName;
-        $methodName = isset($url[1]) ? $url[1] : "index";
+        $methodName = isset($url[1]) ? $url[1] : "index";  //若不存在第二個參數，則呼叫名為index的method
         if (!method_exists($controller, $methodName))
         {
             require_once(dirname(__FILE__)."/error404.html");
@@ -23,8 +23,10 @@ class App {
         $params = $url ? array_values($url) : Array();
         call_user_func_array(Array($controller, $methodName), $params); //這行CALL method
     }
-    
-    public function parseUrl() {
+    //網址的參數作切割，例如https://job-qwerrtty.c9users.io/project_test2/index/membercarpool
+    // 把index/membercarpool切出來，$url[0]即是index，亦是Controller的名稱
+    //membercarpool是$url[1]，即indexController裡的membercarpool method
+    public function parseUrl() {    
         if (isset($_GET["url"])) {
             $url = rtrim($_GET["url"], "/");
             $url = explode("/", $url);
