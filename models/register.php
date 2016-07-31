542c42779ff438dcfbdd62665ac7d9f278daff5a
silent <mailforjob6910@gmail.com>
2016-07-31 11:59:51 +0000

    新增偵錯機制

--- models/register.php
+++ models/register.php
@@ -6,6 +6,7 @@
         {
             $this->connect=new databaseuse;
             $this->DB=$this->connect->DB;
+            $this->DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         function __destruct()
         {
@@ -20,19 +21,28 @@
         }
         function register()
         {
-            $PW=md5($_POST['Password']);
-            $_POST['Account']=htmlentities($_POST['Account']);      //將特殊字元轉碼
-            $_POST['nickname']=htmlentities($_POST['nickname']);
-            $this->DB->query("INSERT INTO `User_AC_PW`
-            (`Account`, `Password`) VALUES ('{$_POST['Account']}'
-            ,'{$PW}')");
-            $this->DB->query("INSERT INTO `User_data`
-            (`Account`, `Gender`, `Nickname`, `E-mail`)
-            Values(
-            '{$_POST['Account']}',
-            '{$_POST['gender']}',
-            '{$_POST['nickname']}',
-            '{$_POST['e-mail']}')");
+            try{
+                $this->DB->beginTransaction();
+                $PW=md5($_POST['Password']);
+                $_POST['Account']=htmlentities($_POST['Account']);      //將特殊字元轉碼
+                $_POST['nickname']=htmlentities($_POST['nickname']);
+                $this->DB->query("INSERT INTO `User_AC_PW`
+                (`Account`, `Password`) VALUES ('{$_POST['Account']}'
+                ,'{$PW}')");
+                $this->DB->query("INSERT INTO `User_data`
+                (`Account`, `Gender`, `Nickname`, `E-mail`)
+                Values(
+                '{$_POST['Account']}',
+                '{$_POST['gender']}',
+                '{$_POST['nickname']}',
+                '{$_POST['e-mail']}')");
+                $this->DB->commit();
+            }
+            catch (PDOException $err) {
+            	$this->DB->rollback();//回溯
+            	error_log($err->getMessage()."\n",3,"./php_errors.log"); //將錯誤寫入log裡
+                return ("error");
+            }
         }
     }
 ?>
