681495f3ff2664f9628e2c031f0e028b147c06d3
silent <mailforjob6910@gmail.com>
2016-08-01 13:58:30 +0000

    上傳圖片

--- testhref.php
+++ testhref.php
@@ -1,11 +1,20 @@
 <?php
+        try{
+        $ID="1";
+        $Account='sss';
+        $test = new PDO("mysql:host=localhost;dbname=project",
+        "root","asd456") ;
+        $test->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $test->beginTransaction();
+        $test->exec("set names utf8");
+        $test->exec("insert into Carpool_ID_AC values(1,'aa')");
+        $test->exec("INSERT INTO  `Carpool_ID_AC` (`ID`,`Account`) 
+            qdqqqdqt}'");
+        $test->commit();
+        $test=null;
+        }
+        catch (PDOException $err) {
+            $test->rollback();
+                echo $err->getMessage();
+            }
 ?>
-<!DOCTYPE html>
-<html>
-    <body>
-        <form method="POST" action=>
-            <input type="text" name="Account"/>
-            <input type="submit" name="OK" value="Submit"/>
-        </form>
-    </body>
-</html>
